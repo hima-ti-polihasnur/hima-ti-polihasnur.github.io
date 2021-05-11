4b38482a0e5e7d543084108df73085d972075fe5
Dev HIMA TI Polhasnur <78461043+hima-ti-polihasnur@users.noreply.github.com>
2021-05-11 11:46:38 +0800

    Update render_ajax.js
    Add atribut loading every tag img

--- js/render_ajax.js
+++ js/render_ajax.js
@@ -2,7 +2,7 @@
 const template = (id, img, vid, nama, jabatan, nim, ttl, angkatan, namaPanggilan, quote, hobi) => {
   $('#'+id).append(`
   <div class="card m-3 shadow" style="width: 18rem;">
-      <img src="${img}" style="width: 100%; class="card-img-top" alt="...">
+      <img src="${img}" style="width: 100%; class="card-img-top" loading="lazy" alt="${nama}_photo">
       <div class="card-body text-center">
        <h5 class="card-title">${nama}</h5>
        <h5 class="card-subtitle text-muted mb-2" style="font-size: .9em!important">${jabatan}</h5>
